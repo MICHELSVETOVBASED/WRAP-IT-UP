05850f5d0a4ef2f6026d35c901cb5cd37c73cd4e
MICHELSVETOVBASED <147500401+MICHELSVETOVBASED@users.noreply.github.com>
2023-12-16 16:30:59 +0100

    DUDEPLS

--- Druid.py
+++ Druid.py
@@ -1,12 +1,14 @@
-x = int(input())
-l = []
-for i in range(x):
-    row =[]
-    for j in range(x):
-        if j == x-1:
-            row.append(5)
-        else : 
-            row.append(1)
-    l.append(row)
-for row in l:
-    print(*row)
+import sys
+
+# Считываем строки из входного потока и удаляем начальные и конечные пробелы
+lst_in = list(map(str.strip, sys.stdin.readlines()))
+
+# Преобразуем пробелы в дефисы для каждой строки
+result_urls = []
+for url in lst_in:
+    replaced_url = '-'.join(url.split())
+    result_urls.append(replaced_url)
+
+# Выводим результат
+for result_url in result_urls:
+    print(result_url)
